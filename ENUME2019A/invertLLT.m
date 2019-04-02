be7e99fabe201cc013038eb43dec8f2dd8f8318e
Ernest Pokropek <34246636+erno98@users.noreply.github.com>
2019-04-02 22:14:27 +0200

    Add files via upload

--- ENUME2019A/invertLLT.m
+++ ENUME2019A/invertLLT.m
@@ -4,19 +4,27 @@ function inverted = invertLLT(A)
     s = s(1);
 
     % LLT factorization
-    L = chol(A);
+    LT = chol(A);
+    L = transpose(LT);
     
     % creating identity matrix
     I = eye(s);
     
-    y = zeros(s);
+    inverted = zeros(s);
+    X = zeros(s);
     
-    for i = 1:s
-         y(:, i) = L'*L \ I(:, i);
+    for k=1:s
+        X(1,k) = I(1,k)./L(1,1);
+        for m=2:s
+            X(m,k) = (I(m,k)-L(m,1:m-1)*X(1:m-1,k))./L(m,m);
+        end
+        
+        inverted(s,k) = X(s,k)./LT(s,s);
+        
+        for m=s-1:-1:1
+            inverted(m,k) = (X(m,k)-LT(m,m+1:s)*inverted(m+1:s,k))./LT(m,m);
+        end
     end
-    
-   	inverted = y;
-    
     
 end
 
--- ENUME2019A/invertLU.m
+++ ENUME2019A/invertLU.m
@@ -9,13 +9,20 @@ function inverted = invertLU(A)
     % creating identity matrix
     I = eye(s);
     
-    y = zeros(s);
-    
-    for i = 1:s
-         y(:, i) = L*U \ I(:, i);
-    end
+    inverted = zeros(s);
+    X = zeros(s);
 
-    inverted = y;
+    for k=1:s
+        X(1,k) = I(1,k);
+        for m=2:s
+            X(m,k) = (I(m,k)-L(m,1:m-1)*X(1:m-1,k));
+        end
+        
+        inverted(s,k) = X(s,k)./U(s,s);
+        for m=s-1:-1:1
+            inverted(m,k) = (X(m,k)-U(m,m+1:s)*inverted(m+1:s,k))./U(m,m);
+        end
+    end
    
 end
 

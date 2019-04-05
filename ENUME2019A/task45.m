3a5f6e637f33f1abfa9e8f6c080ce4071a692fe3
Ernest Pokropek <34246636+erno98@users.noreply.github.com>
2019-04-05 18:09:57 +0200

    Add files via upload

--- ENUME2019A/task45.m
+++ ENUME2019A/task45.m
@@ -18,6 +18,7 @@ norm20_e = zeros(2, length(k));
 norm20_inf = zeros(2, length(k));
 
 xs = zeros(1, length(k));
+means = zeros(6, length(k));
 
 for i = k+1
     xs(i) = 2^(i-1)/300;
@@ -52,7 +53,18 @@ for i = k+1
     norm20_inf(1, i) = m_norm_inf(A20, invertLU(A20));
     norm20_inf(2, i) = m_norm_inf(A20, invertLLT(A20));
     
+    means(1, i) = mean(abs(inv(A3) - invertLU(A3)), 'All');
+    means(2, i) = mean(abs(inv(A3) - invertLLT(A3)), 'All');
+    
+    means(3, i) = mean(abs(inv(A10) - invertLU(A10)), 'All');
+    means(4, i) = mean(abs(inv(A10) - invertLLT(A10)), 'All');
+    
+    means(5, i) = mean(abs(inv(A20) - invertLU(A20)), 'All');
+    means(6, i) = mean(abs(inv(A20) - invertLLT(A20)), 'All');
+    
 end
+
+
 
 % RMSE on x
 figure(1)
@@ -61,7 +73,6 @@ loglog(xs, RMSE3(1, :), 'r', xs, RMSE3(2, :), '--r')
 hold on
 loglog(xs, RMSE10(1, :), 'b', xs, RMSE10(2, :), '--b')
 loglog(xs, RMSE20(1, :), 'm', xs, RMSE20(2, :), '--m')
-loglog(xs, norm10_inf(1, :), 'k', xs, norm10_inf(2, :), '--k')
 title('RMSE on x')
 xlabel('x')
 ylabel('RMSE')
@@ -74,27 +85,62 @@ loglog(xs, ME3(1, :), 'r', xs, ME3(2, :), '--r')
 hold on
 loglog(xs, ME10(1, :),'b',  xs, ME10(2, :), '--b')
 loglog(xs, ME20(1, :), 'm', xs, ME20(2, :), '--m')
-loglog(xs, (norm3_e(1, :) + norm10_e(1, :) + norm20_e(1, :))/3, 'k', xs, norm10_e(2, :), '--k')
 title('ME on x')
 xlabel('x')
 ylabel('ME')
-legend('LU N=3', 'LLT N=3', 'LU N=10', 'LLT N=10', 'LU N=20', 'LLT N=20','avg norm LU', 'norm LLT', 'Location', 'northwest')
+legend('LU N=3', 'LLT N=3', 'LU N=10', 'LLT N=10', 'LU N=20', 'LLT N=20', 'Location', 'northwest')
 hold off
 
-% comparison with norm
+% check norm 
+norm_euc_diff_3_LU = mean(abs(ME3(1, :) - norm3_e(1, :)));
+norm_euc_diff_10_LU = mean(abs(ME10(1, :) - norm10_e(1, :)));
+norm_euc_diff_20_LU = mean(abs(ME20(1, :) - norm20_e(1, :)));
+
+norm_inf_diff_3_LU = mean(abs(RMSE3(1, :) - norm3_inf(1, :)));
+norm_inf_diff_10_LU = mean(abs(RMSE10(1, :) - norm10_inf(1, :)));
+norm_inf_diff_20_LU = mean(abs(RMSE20(1, :) - norm20_inf(1, :)));
+
+norm_euc_diff_3_LLT = mean(abs(ME3(2, :) - norm3_e(2, :)));
+norm_euc_diff_10_LLT = mean(abs(ME10(2, :) - norm10_e(2, :)));
+norm_euc_diff_20_LLT = mean(abs(ME20(2, :) - norm20_e(2, :)));
+
+norm_inf_diff_3_LLT = mean(abs(RMSE3(2, :) - norm3_inf(2, :)));
+norm_inf_diff_10_LLT = mean(abs(RMSE10(2, :) - norm10_inf(2, :)));
+norm_inf_diff_20_LLT = mean(abs(RMSE20(2, :) - norm20_inf(2, :)));
+
+c = categorical({'euclidan-LU', 'infinite-LU', 'euclidan-LLT', 'infinite-LLT'});
+norms = [norm_euc_diff_3_LU, norm_euc_diff_10_LU, norm_euc_diff_20_LU; ...
+    norm_inf_diff_3_LU, norm_inf_diff_10_LU, norm_inf_diff_20_LU; ...
+    norm_euc_diff_3_LLT, norm_euc_diff_10_LLT, norm_euc_diff_20_LLT; ...
+    norm_inf_diff_3_LLT, norm_inf_diff_10_LLT, norm_inf_diff_10_LLT
+    ];
+
 figure(3)
-diff_e_LU = abs((norm3_e(1, :) - ME3(1, :)) + (norm10_e(1, :) - ME10(1, :)) + (norm20_e(1, :) - ME20(1, :))) / 3;
-diff_e_LLT = abs((norm3_e(2, :) - ME3(2, :)) + (norm10_e(2, :) - ME10(2, :)) + (norm20_e(2, :) - ME20(2, :))) / 3;
-diff_inf_LU = abs((norm3_inf(1, :) - RMSE3(1, :)) + (norm10_inf(1, :) - RMSE10(1, :)) + (norm20_inf(1, :) - RMSE20(1, :))) / 3;
-diff_inf_LLT = abs((norm3_inf(2, :) - RMSE3(2, :)) + (norm10_inf(2, :) - RMSE10(2, :)) + (norm20_inf(2, :) - RMSE20(2, :))) / 3;
-loglog(xs, diff_e_LU, 'ko')
+bar(c, norms)
 hold on
-loglog(xs, diff_e_LLT, 'ro')
-loglog(xs, diff_inf_LU, 'k.')
-loglog(xs, diff_inf_LLT, 'r.')
-xlabel('x')
-ylabel('mean difference')
-legend('norm-ME LU', 'norm-ME LLT', 'norm.inf-RMSE LU', 'norm.inf-RMSE LLT')
+ylabel("|matlab's norm-norm|")
+xlabel('norm-decomposition')
+legend('N=3', 'N=10', 'N=20', 'Location', 'northwest')
+grid on
+hold off
+
+% comparison to inv
+figure(4)
+means_to_bar = [mean(means(1, :)), mean(means(2, :)); ...
+                mean(means(3, :)), mean(means(4, :)); ...
+                mean(means(5, :)), mean(means(6, :))
+];
+
+c = categorical({'N=3', 'N=10', 'N=20'});
+c = reordercats(c, {'N=3', 'N=10', 'N=20'});
+
+bar(c, means_to_bar)
+hold on
+set(gca,'YScale','log')
+grid on
+title("average difference between inverse and inv()")
+ylabel('difference')
+legend('LU', 'LLT', 'Location', 'northeast')
 hold off
 
 function rmse = norm_2(A, A_estimate)

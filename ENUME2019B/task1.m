81b4e07a4cd4546ad45cc25891c9e17a1304775e
Ernest Pokropek <34246636+erno98@users.noreply.github.com>
2019-04-15 15:01:15 +0200

    task1

---
+++ ENUME2019B/task1.m
@@ -0,0 +1,20 @@
+
+% plotting the function
+fplot(@(x) f(x), [-1, 1])
+
+% indicating sequences
+s10 = indicate(10);
+s20 = indicate(20);
+s30 = indicate(30);
+
+function y = f(x)
+    y = (x+1/3)^2 + exp(-x-2);
+end
+
+function sequence = indicate(N)
+    sequence = zeros(1, N);
+    for n = 1:N
+       x = -1 + 2*(n-1)/(N-1);
+       sequence(n) = f(x);
+    end
+end
